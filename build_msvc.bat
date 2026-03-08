257c8a4bda1536a42230850333bf18d0291c7d07
4equest <107108812+4equest@users.noreply.github.com>
2026-03-08 17:01:13 +0900

    ci

--- build_msvc.bat
+++ build_msvc.bat
@@ -1,4 +1,37 @@
 @echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-cmake -B build -A x64
+setlocal
+
+set "VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if not exist "%VSWHERE%" (
+    echo Could not find vswhere.exe>&2
+    exit /b 1
+)
+
+set "VSINSTALL="
+for /f "usebackq tokens=*" %%I in (`"%VSWHERE%" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+    set "VSINSTALL=%%I"
+)
+
+if not defined VSINSTALL (
+    echo Could not locate a Visual Studio installation with C++ tools.>&2
+    exit /b 1
+)
+
+pushd "%~dp0"
+call "%VSINSTALL%\VC\Auxiliary\Build\vcvars64.bat"
+if errorlevel 1 goto :build_failed
+
+cmake -S . -B build -A x64
+if errorlevel 1 goto :build_failed
+
 cmake --build build --config Release
+if errorlevel 1 goto :build_failed
+
+popd
+endlocal
+exit /b 0
+
+:build_failed
+set "status=%ERRORLEVEL%"
+popd
+endlocal & exit /b %status%
